d128b33b4111132f7f89e5dad0d3bd3668524b0f
kknd8189 <56291895+kknd8189@users.noreply.github.com>
2022-03-07 13:32:13 +0900

    Initial Commit

---
+++ D3D/playGround.cpp
@@ -0,0 +1,56 @@
+#include "pch.h"
+#include "playGround.h"
+
+
+playGround::playGround()
+{
+}
+
+
+playGround::~playGround()
+{
+}
+
+//초기화는 여기에다 해라!!!
+HRESULT playGround::init()
+{
+	gameNode::init(true);
+
+	SCENEMANAGER->addScene("땅끄씬", new tankScene);
+	SCENEMANAGER->changeScene("땅끄씬");
+
+
+	return S_OK;
+}
+
+//메모리 해제는 여기다 해라!!!!
+void playGround::release()
+{
+	gameNode::release();
+
+
+}
+
+//연산처리는 여기다가!
+void playGround::update()
+{
+	gameNode::update();
+
+	SCENEMANAGER->update();
+	
+}
+
+//여기다 그려줘라!!!
+void playGround::render()
+{
+	PatBlt(getMemDC(), 0, 0, WINSIZEX, WINSIZEY, WHITENESS);
+	//==============위에는 제발 건드리지 마라 ============
+
+	SCENEMANAGER->render();
+
+
+	//=============== 밑에도 건들지마라 ================
+	_backBuffer->render(getHDC(), 0, 0);
+
+}
+
